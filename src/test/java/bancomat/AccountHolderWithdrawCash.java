859cd1e31ad71398df5d1b53aff75645eeacd318
slezier <sylvain.lezier@orange.com>
2014-04-07 13:39:07 +0200

    Add 2 scenarios

--- src/test/java/bancomat/AccountHolderWithdrawCash.java
+++ src/test/java/bancomat/AccountHolderWithdrawCash.java
@@ -7,6 +7,7 @@ import sft.SimpleFunctionalTest;
 import sft.Text;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -38,6 +39,28 @@ public class AccountHolderWithdrawCash {
         andTheCardShouldBeReturned();
     }
 
+    @Test
+    public void accountHasInSufficientFunds(){
+        givenTheAccountBalanceIs10Dollars();
+        andTheCardIsValid();
+        andTheMachineContainsEnoughMoney();
+
+        whenTheAccountHolderRequests20Dollars();
+
+        thenTheAtmShouldNotDispenseAnyMoney();
+        andTheAtmShouldDisplayInsufficientFunds();
+        andTheAccountBalanceShouldBe10Dollars();
+        andTheCardShouldBeReturned();
+    }
+
+    @Test
+    public void  cardHasBeenDisabled(){
+        givenTheCardIsDisabled();
+        whenTheAccountHolderRequests20Dollars();
+        thenTheAtmShouldRetainTheCard();
+        andTheAtmShouldDisplayTheCardHasBeenRetained();
+    }
+
     private void andTheCardShouldBeReturned() {
         assertTrue("Card not returned", atm.returnCard());
     }
@@ -46,12 +69,27 @@ public class AccountHolderWithdrawCash {
     private void andTheAccountBalanceShouldBe80Dollars() {
         assertEquals(account.balance(), 80);
     }
+    @Text("And the account balance should be 10$")
+    private void andTheAccountBalanceShouldBe10Dollars() {
+        assertEquals(account.balance(), 10);
+    }
 
     @Text("Then the atm should dispense 20$")
     private void thenTheAtmShouldDispense20Dollars() {
         assertEquals(withdrawals, 20);
     }
 
+    private void thenTheAtmShouldRetainTheCard() {
+        assertFalse("Card returned", atm.returnCard());
+    }
+
+    private void andTheAtmShouldDisplayInsufficientFunds() {
+        assertEquals(atm.getDisplay(), "Insufficient funds");
+    }
+
+    private void andTheAtmShouldDisplayTheCardHasBeenRetained() {
+        assertEquals(atm.getDisplay(), "The card has been retained");
+    }
     @Text("When the account holder requests 20$")
     private void whenTheAccountHolderRequests20Dollars() {
         atmSession = atm.authenticate(user);
@@ -71,6 +109,22 @@ public class AccountHolderWithdrawCash {
         bank = new Bank();
         user = new User();
         account = bank.createAccount(user, 100);
+    }
+
+    @Text("Given the account balance is 10$")
+    private void givenTheAccountBalanceIs10Dollars() {
+        bank = new Bank();
+        user = new User();
+        account = bank.createAccount(user, 10);
+    }
+
+    private void givenTheCardIsDisabled() {
+        account.addValidCreditCard("1234");
+        account.declareCardLoss();
+    }
+
+    private void thenTheAtmShouldNotDispenseAnyMoney() {
+        assertEquals(withdrawals,0);
     }
 
 }
